96774bdf65c19f3484bee8e6095c14818b498773
mpleyer <mpleyer@student.tgm.ac.at>
2017-10-12 11:58:20 +0200

    first delete
    delete all

--- python/Bildschirmschoner/build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to A01 Schneeflocke\u2019s documentation!"],titleterms:{a01:0,document:0,indic:0,schneeflock:0,tabl:0,welcom:0}})
